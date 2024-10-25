ff31ca20ebcceaff1700af9d94496165cd196193
Jparedest1 <jhony.paredes1994@gmail.com>
2024-10-24 22:49:18 -0600

    Fix CORS

--- VentasDB/Controllers/BitacoraController.cs
+++ VentasDB/Controllers/BitacoraController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VentasDB.Models;
@@ -10,6 +11,7 @@ namespace VentasDB.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("CorsPolicy")]
     public class BitacoraController : ControllerBase
     {
         private readonly VentasDBContext _context;
